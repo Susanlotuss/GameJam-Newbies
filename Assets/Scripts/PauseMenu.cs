86edf7bf14d19b96d7b1d3e31f512446e4120a01
Elia Luque <luqueliux@gmail.com>
2022-08-21 14:40:41 -0500

    Avance menu, UI

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject botonPausa;
+
+    [SerializeField] private GameObject menuPausa;
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+        botonPausa.SetActive(false);
+        menuPausa.SetActive(true);
+    }
+
+    public void Reunudar()
+    {
+        Time.timeScale = 1f;
+        botonPausa.SetActive(true);
+        menuPausa.SetActive(false);
+    }
+
+    public void Reiniciar()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void MenuPrincipal()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("StartMenu");
+    }
+}
